acbb0bc71f4cd73a0131e1385d8f18dcc857e72d
Umang <umangagarwal345@gmail.com>
2022-06-30 18:40:45 +0530

    Added user.core as a dependency in user.command and user.query

--- user-management/user.query.api/src/main/java/com/umang345/user/query/api/UserQueryApplication.java
+++ user-management/user.query.api/src/main/java/com/umang345/user/query/api/UserQueryApplication.java
@@ -1,9 +1,12 @@
 package com.umang345.user.query.api;
 
+import com.umang345.user.core.configuration.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({AxonConfig.class})
 public class UserQueryApplication {
 
 	public static void main(String[] args) {
