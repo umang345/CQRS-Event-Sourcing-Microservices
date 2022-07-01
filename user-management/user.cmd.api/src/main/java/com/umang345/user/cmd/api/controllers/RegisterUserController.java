301fe7c7068633ef76ec0475a31e5fd6b55aeb04
Umang <umangagarwal345@gmail.com>
2022-07-01 14:47:23 +0530

    Added PostMapping annotation to the endpoint

--- user-management/user.cmd.api/src/main/java/com/umang345/user/cmd/api/controllers/RegisterUserController.java
+++ user-management/user.cmd.api/src/main/java/com/umang345/user/cmd/api/controllers/RegisterUserController.java
@@ -6,10 +6,12 @@ import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
@@ -24,18 +26,20 @@ public class RegisterUserController
          this.commandGateway = commandGateway;
     }
 
-    public ResponseEntity<RegisterUserResponse> registerUser(@RequestBody RegisterUserCommand command)
+    @PostMapping
+    public ResponseEntity<RegisterUserResponse> registerUser(@Valid @RequestBody RegisterUserCommand command)
     {
-        command.setId(UUID.randomUUID().toString());
+        var id = UUID.randomUUID().toString();
+        command.setId(id);
 
         try {
-            commandGateway.sendAndWait(command);
-            return new ResponseEntity<>(new RegisterUserResponse("User Successfully Registered"), HttpStatus.CREATED);
+            commandGateway.send(command);
+            return new ResponseEntity<>(new RegisterUserResponse(id,"User Successfully Registered"), HttpStatus.CREATED);
         }
         catch (Exception e){
-            var safeErrorMessage = "Error while processing register user request for id - "+command.getId();
+            var safeErrorMessage = "Error while processing register user request for id - "+id;
             System.out.println(e.getMessage());
-            return new ResponseEntity<>(new RegisterUserResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new RegisterUserResponse(id,safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
