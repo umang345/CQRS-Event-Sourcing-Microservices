ee3d01c821111ee5ee055e82da3817236a70c393
Umang <umangagarwal345@gmail.com>
2022-07-01 13:52:29 +0530

    Added Bean validation annotations on User model fields

--- user-management/user.core/src/main/java/com/umang345/user/core/models/User.java
+++ user-management/user.core/src/main/java/com/umang345/user/core/models/User.java
@@ -7,6 +7,10 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,8 +20,12 @@ public class User
 {
     @Id
     private String id;
+    @NotEmpty(message = "firstname is mandatory")
     private String firstname;
+    @NotEmpty(message = "lastname is mandatory")
     private String lastname;
+    @Email(message = "Please provide a valid email address")
     private String emailAddress;
+    @NotNull(message = "Please provide account credentials")
     private Account account;
 }
