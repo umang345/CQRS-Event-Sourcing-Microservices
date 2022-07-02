d824bea36e89df1256048579dcb399edbbed5ff9
Umang <umangagarwal345@gmail.com>
2022-07-02 10:17:11 +0530

    OAuth2 Service initial setup

---
+++ user-management/user.oAuth2.0/src/main/java/com/umang345/user/oAuth20/Application.java
@@ -0,0 +1,15 @@
+package com.umang345.user.oAuth20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@SpringBootApplication
+@EnableAuthorizationServer
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
