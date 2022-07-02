565455ce12e1b1f6cfd8fbecfff29b859484358a
Umang <umangagarwal345@gmail.com>
2022-07-02 18:12:15 +0530

    Bank Account Service - Initial Project Setup

---
+++ bank-account/bankacc.cmd.api/src/test/java/com/umang345/bankacc/cmd/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umang345.bankacc.cmd.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
