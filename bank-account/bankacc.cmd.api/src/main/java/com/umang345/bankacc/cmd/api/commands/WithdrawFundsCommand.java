e7d26b5ee4aea04c0ec99c09a0173969844fc502
Umang <umangagarwal345@gmail.com>
2022-07-03 09:50:18 +0530

    Added Rest Controller for withdrawl of funds from Bank Account

--- bank-account/bankacc.cmd.api/src/main/java/com/umang345/bankacc/cmd/api/commands/WithdrawFundsCommand.java
+++ bank-account/bankacc.cmd.api/src/main/java/com/umang345/bankacc/cmd/api/commands/WithdrawFundsCommand.java
@@ -4,11 +4,15 @@ import lombok.Builder;
 import lombok.Data;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
+import javax.validation.constraints.Min;
+
 @Data
 @Builder
 public class WithdrawFundsCommand
 {
     @TargetAggregateIdentifier
     private String id;
+
+    @Min(value = 1, message = "Withdraw amount must be greater than 0.")
     private double amount;
 }
---
+++ bank-account/bankacc.cmd.api/src/main/java/com/umang345/bankacc/cmd/api/controllers/WithdrawFundsController.java
@@ -0,0 +1,65 @@
+package com.umang345.bankacc.cmd.api.controllers;
+
+import com.umang345.bankacc.cmd.api.commands.WithdrawFundsCommand;
+import com.umang345.bankacc.core.dto.BaseResponse;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(path="/api/v1/withdrawFunds")
+public class WithdrawFundsController
+{
+    private final CommandGateway commandGateway;
+
+    @Autowired
+    public WithdrawFundsController(CommandGateway commandGateway)
+    {
+        this.commandGateway = commandGateway;
+    }
+
+    @PutMapping(path = "/{id}")
+    @PreAuthorize("hasAuthority('WRITE_PRIVILEGE')")
+    public ResponseEntity<BaseResponse> withdrawFunds(@PathVariable(value = "id") String id,
+                                                     @Valid @RequestBody WithdrawFundsCommand command)
+    {
+        try {
+            command.setId(command.getId());
+            commandGateway.send(command).get();
+
+            return new ResponseEntity<>(new BaseResponse("Withdrawl successfully completed"), HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            var safeErrorMessage = "Error while processing request to withdraw funds from bank account for id - "+id;
+            System.out.println(e.toString());
+
+            return new ResponseEntity<>(new BaseResponse(safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
